cf9cd9e8eed42de9cae1b6a5887264224cffb263
RiyazAhamed-1489 <135771387+RiyazAhamed-1489@users.noreply.github.com>
2023-12-15 09:21:57 -0500

    Add files via upload
    initial commit

---
+++ persistence-service-postgres-orm/persistence-service-postgres-orm/backend/src/strategy/postgresql/configure.ts
@@ -0,0 +1,15 @@
+import { DataSource } from "typeorm";
+import { config } from 'dotenv';
+import { Photo } from "./photo/photo";
+
+export const postgresDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [Photo],
+  synchronize: true,
+  logging: false,
+});
